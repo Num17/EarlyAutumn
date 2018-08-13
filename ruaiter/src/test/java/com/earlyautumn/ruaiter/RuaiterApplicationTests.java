6f2ebdb898f5627845588374ff5679b5b16c5352
17 <2540705109@qq.com>
2018-08-13 22:06:18 +0800

    mysql redis no test

--- ruaiter/src/test/java/com/earlyautumn/ruaiter/RuaiterApplicationTests.java
+++ ruaiter/src/test/java/com/earlyautumn/ruaiter/RuaiterApplicationTests.java
@@ -3,11 +3,14 @@ package com.earlyautumn.ruaiter;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class RuaiterApplicationTests {
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@SpringBootTest(classes = RuaiterApplicationTests.class, webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+public abstract class RuaiterApplicationTests {
 
     @Test
     public void contextLoads() {
